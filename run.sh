9aedaee2bda92b21137516107c5b1d83ab7768e7
adamws <adamws@users.noreply.github.com>
2019-02-16 10:46:06 +0000

    conditional mount of ttyUSB1

--- run.sh
+++ run.sh
@@ -3,17 +3,25 @@
 FTDI_USB_INFO=$(lsusb | grep "0403:6010")
 
 if [ -z "$FTDI_USB_INFO" ]; then
-	echo "FTDI device not found, running without debug support"
+	echo "FTDI device not found, running without flashing and debug support"
 else
 	FTDI_BUS=$(echo $FTDI_USB_INFO | sed 's/Bus \([0-9]*\).*/\1/')
 	FTDI_DEVICE=$(echo $FTDI_USB_INFO | sed 's/.*Device \([0-9]*\).*/\1/')
+	FTDI_DEVICE_MOUNT="--device /dev/bus/usb/${FTDI_BUS}/${FTDI_DEVICE}"
+
+	if [ -c /dev/ttyUSB1 ]; then
+		FTDI_TTY_MOUNT="--device /dev/ttyUSB1:/dev/ttyUSB1:rwm"
+	fi
 fi
+
 
 DOCKER_IMAGE=$1
 shift 1
 VOLUMES="$*"
 
-docker run -it --rm --device /dev/bus/usb/${FTDI_BUS}/${FTDI_DEVICE} \
-	--device /dev/ttyUSB1:/dev/ttyUSB1:rwm \
+docker run -it --rm \
+	${FTDI_DEVICE_MOUNT} \
+	${FTDI_TTY_MOUNT} \
 	${VOLUMES} \
 	${DOCKER_IMAGE}
+
